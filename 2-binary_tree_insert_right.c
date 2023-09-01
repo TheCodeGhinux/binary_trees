cbb48e4cd20880c3d67d0f1f91b8ba076432247f
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-01 09:27:02 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,25 +1,33 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ *binary_tree_insert_left - To create a binary node
+ *	as a right child
+ *@parent: Pointer to the parent of node to insert
+ *@value: Value of new node
+ *Return: Pointer of new node else Null on error
+ */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-  if (parent == NULL)
-    return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-  new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
-  if (new_node == NULL)
-    return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-  if (parent->right != NULL)
-  {
-    new_node->right = parent->right;
-    parent->right->parent = new_node;
-  }
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 
-  parent->right = new_node;
+	parent->right = new_node;
 
-  return (new_node);
+	return (new_node);
 }
